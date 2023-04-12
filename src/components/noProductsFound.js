556453e31ce5ef62d47f1932b3268732550198f0
MosElsh <moselsh1@gmail.com>
2023-04-12 16:55:47 +0100

    Separated No Products Found Component

---
+++ src/components/noProductsFound.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const NoProductsFound = (props) => {
+    return (
+        <section id="no-products-found">
+            No Products Found. Please clear some filters.
+        </section>
+    )
+}
